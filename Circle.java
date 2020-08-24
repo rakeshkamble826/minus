f32c902f1efbfb65ce910295ec4e0ba111c56b14
rakeshkamble826 <70163455+rakeshkamble826@users.noreply.github.com>
2020-08-24 23:50:25 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,12 @@
+
+public class Circle {
+	public static void main (String[]args){
+		int r=5;
+		double radius, area;
+		area=3.14*5*5;
+		
+		
+		System.out.println("Area of Circus is:"+area);
+		System.out.println("Circumference of circle is:"+area);
+	}
+}
